188d02db1b59b3ea053eadaf14cf2b7a00be76b3
lispringing <2638526782@qq.com>
2026-03-18 13:19:19 +0800

    Update ddr_test_top.v

--- board_3_oneboard_design/source/ddr_test_top.v
+++ board_3_oneboard_design/source/ddr_test_top.v
@@ -579,6 +579,11 @@ ov5640_power_on_delay	power_on_delay_inst(
 	.camera_pwnd             (               ),//output
 	.initial_en              (initial_en     ) //output		
 );
+// ============== 第2步：强制两路 OV5640 都启动 ==============
+assign initial_en = 1'b1;      // 强制初始化
+assign cmos1_reset = 1'b1;     // 强制复位高
+assign cmos2_reset = 1'b1;     // 强制复位高
+// =========================================================
 //CMOS 8bitת16bit//////////
 //CMOS1
 always@(posedge cmos1_pclk)
@@ -700,7 +705,7 @@ user_axi_m_arbitration (
 //fifo2�źţ���CMOS1                                  
     .video2_clk_in           (cmos1_pclk_16bit),                       
     .video2_de_in            (cmos1_href_16bit ),
-    .video2_data_in          ({cmos1_d_16bit[4:0],5'b0,cmos1_d_16bit[10:5],4'b0,cmos1_d_16bit[15:11],7'b0}),//27
+    .video2_data_in          ({cmos1_d_16bit[15:11],3'b0, cmos1_d_16bit[10:5],2'b0, cmos1_d_16bit[4:0],3'b0}),  // R5 G6 B5 正确顺序
     .video2_rd_en            (video2_rd_en   ),
     .video2_data_out         (video2_data_out),
     .fram2_done              (fram2_done     ),
@@ -708,7 +713,7 @@ user_axi_m_arbitration (
 //fifo3�ź�                                        
     .video3_clk_in           (cmos2_pclk_16bit),                       
     .video3_de_in            (cmos2_href_16bit    ),
-    .video3_data_in          ({cmos2_d_16bit[4:0],5'b0,cmos2_d_16bit[10:5],4'b0,cmos2_d_16bit[15:11],7'b0}  ),
+    .video3_data_in          ({cmos2_d_16bit[15:11],3'b0, cmos2_d_16bit[10:5],2'b0, cmos2_d_16bit[4:0],3'b0}),
     .video3_rd_en            (video3_rd_en   ),
     .video3_data_out         (video3_data_out),
     .fram3_done              (fram3_done     ),
